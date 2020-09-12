a0de7603807f7eb1c68b35dd82a72d2f1a80240d
Anjanay8055 <70766862+Anjanay8055@users.noreply.github.com>
2020-09-12 15:31:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var fr,mr;
+function setup() {
+  createCanvas(800,400);
+  mr=createSprite(400, 200, 50, 50);
+  fr=createSprite(400, 200, 50, 50);
+  mr.shapeColor = "yellow"; 
+  fr.shapeColor = "yellow"; 
+}
+function draw() {
+  background(255,255,255);
+ mr.x=mouseX;
+ mr.y=mouseY;
+  if((mr.x-fr.x<mr.width/2 + fr.width/2 )&&
+    (fr.x-mr.x<mr.width/2 + fr.width/2 )&&
+    ( mr.y-fr.y<mr.height/2 + fr.height/2 )&&
+    (fr.y-mr.y<mr.height/2 + fr.height/2 )){
+    mr.shapeColor = "orange"; 
+    fr.shapeColor = "green"; 
+  }
+  else{
+    mr.shapeColor = "yellow"; 
+    fr.shapeColor = "yellow"; 
+  }
+   drawSprites();
+}
